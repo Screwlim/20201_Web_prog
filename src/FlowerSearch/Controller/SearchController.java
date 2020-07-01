1fad3f7969750b09393f063adf445e319042c38b
pcrmcw0486 <39568531+pcrmcw0486@users.noreply.github.com>
2020-07-01 18:21:31 +0900

    db수정
    db수정

--- src/FlowerSearch/Controller/SearchController.java
+++ src/FlowerSearch/Controller/SearchController.java
@@ -1,12 +1,17 @@
 package FlowerSearch.Controller;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import FlowerSearch.model.FlowerDAO;
+import FlowerSearch.model.FlowerDTO;
 
 public final class SearchController extends HttpServlet {
 
@@ -25,11 +30,23 @@ public final class SearchController extends HttpServlet {
 		response.setContentType("text/html; charset=utf-8");
 		request.setCharacterEncoding("utf-8");
 		if(command.equals("/Search/SearchByOptions.do")) {
-			System.out.println("??!");
+			requestSearchByOptions(request);
 			RequestDispatcher rd = request.getRequestDispatcher("search_flower.jsp");
 			rd.forward(request, response);
 		}
 		System.out.println(command);
 	};
 
+	public void requestSearchByOptions(HttpServletRequest request) {
+		FlowerDAO dao = FlowerDAO.getInstnace();
+		List<FlowerDTO> flowerlist = new ArrayList<FlowerDTO>();
+		
+		//request로 부터 파라미터 값 받아오기
+		String[] season_param = request.getParameterValues("season");
+		String[] color_param = request.getParameterValues("color");
+		String search_name = String.valueOf(request.getAttribute("search_name"));
+		flowerlist = dao.serachByOptions(search_name, season_param, color_param);
+		System.out.println(flowerlist);
+		request.setAttribute("SearchResult", flowerlist);
+	}
 }
--- src/FlowerSearch/database/DBConnection.java
+++ src/FlowerSearch/database/DBConnection.java
@@ -10,12 +10,13 @@ public class DBConnection {
 	public static Connection getConnection() throws SQLException, ClassNotFoundException{
 		Connection conn = null;
 		//connection information
-
-		String url = "jdbc:oracle:thin:@localhost:1521:xe";
+		 
+		Class.forName("oracle.jdbc.driver.OracleDriver");
+		String url = "jdbc:oracle:thin:@localhost:1521:orcl";
 		String user = "webdb";
 		String password = "webdb";
 		
-	    Class.forName("oracle.jdbc.driver.OracleDriver");
+	   
 
 		conn = DriverManager.getConnection(url, user , password);
 		

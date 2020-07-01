2c3398855ab920d1c73a3d7bc80bdf945f79e5c3
Sukryul Lim <srlim1121@gmail.com>
2020-07-01 17:15:35 +0900

    more test data added

--- db/webdb data.sql
+++ db/webdb data.sql
@@ -12,6 +12,29 @@ insert into SEASONS values (0, '봄');
 insert into SEASONS values (1, '여름');
 insert into SEASONS values (2, '가을');
 insert into SEASONS values (3, '겨울');
+------------------------------make SEASON-------------------------------------------------
+Insert into SEASON values (0, 1);
+Insert into SEASON values (1, 1);
+Insert into SEASON values (2, 0);
+Insert into SEASON values (2, 2);
+Insert into SEASON values (3, 0);
+Insert into SEASON values (3, 1);
+Insert into SEASON values (3, 2);
+Insert into SEASON values (3, 3);
+Insert into SEASON values (4, 2);
+Insert into SEASON values (4, 3);
+Insert into SEASON values (5, 1);
+Insert into SEASON values (6, 2);
+Insert into SEASON values (7, 3);
+Insert into SEASON values (8, 0);
+Insert into SEASON values (8, 3);
+Insert into SEASON values (9, 0);
+Insert into SEASON values (10, 1);
+Insert into SEASON values (11, 0);
+Insert into SEASON values (11, 1);
+Insert into SEASON values (11, 2);
+Insert into SEASON values (11, 3);
+
 ------------------------------make KEYWORDS-------------------------------------------------
 insert into KEYWORDS values (0, '희망');
 insert into KEYWORDS values (1, '사랑');
@@ -23,7 +46,18 @@ insert into KEYWORDS values (6, '축하');
 insert into KEYWORDS values (7, '행복');
 insert into KEYWORDS values (8, '축복');
 insert into KEYWORDS values (9, '자랑');
-
+insert into KEYWORDS values (10,'걱정');
+insert into KEYWORDS values (11, '슬픔');
+insert into KEYWORDS values (12, '위로');
+insert into KEYWORDS values (13, '시작');
+insert into KEYWORDS values (14, '존경');
+insert into KEYWORDS values (15, '응원');
+insert into KEYWORDS values (16, '재물');
+insert into KEYWORDS values (17, '분노');
+insert into KEYWORDS values (18, '질투');
+insert into KEYWORDS values (19, '최선');
+insert into KEYWORDS values (20, '건강');
+insert into KEYWORDS values (21, '아름다움');
 
 ------------------------------make USER_INFO-------------------------------------------------
 insert into USER_INFO values ('test', 'test', '김철수', 'M', to_date('1990-05-23','yyyy-mm-dd'), '434123453', '대한민국 대구광역시 북구 대현동 183-3번지', 'test');
@@ -34,6 +68,13 @@ insert into FLOWER(Fname, F_ID, Birth_Month) values ('장미', 1, 0);
 insert into FLOWER(Fname, F_ID, Birth_Month) values ('안개꽃', 2, 0);
 insert into FLOWER(Fname, F_ID, Birth_Month) values ('팜파스 그라스', 3, 0);
 insert into FLOWER(Fname, F_ID, Birth_Month) values ('포인세티아', 4, 0);
+insert into FLOWER(Fname, F_ID, Birth_Month) values ('메리골드', 5, 0);
+insert into FLOWER(Fname, F_ID, Birth_Month) values ('용담', 6, 0);
+insert into FLOWER(Fname, F_ID, Birth_Month) values ('스노우 드롭', 7, 0);
+insert into FLOWER(Fname, F_ID, Birth_Month) values ('크리스마스 로즈', 8, 0);
+insert into FLOWER(Fname, F_ID, Birth_Month) values ('프리지아', 9, 0);
+insert into FLOWER(Fname, F_ID, Birth_Month) values ('작약', 10, 0);
+insert into FLOWER(Fname, F_ID, Birth_Month) values ('금전수', 11, 0);
 
 ------------------------------make CATEGORIZED-------------------------------------------------
 
@@ -44,7 +85,17 @@ insert into CATEGORIZED values (1, 4, 5); // 장미 진분홍
 insert into CATEGORIZED values (2, 3, 1); // 안개꽃 빨강
 insert into CATEGORIZED values (3, 9, 5); // 팜피스 그라스
 insert into CATEGORIZED values (4, 6, 1); // 포인세티아
-insert into CATEGORIZED values (4, 8, 1); // 
+insert into CATEGORIZED values (5, 7, 2); // 메리골드
+insert into CATEGORIZED values (6, 1, 3); // 
+insert into CATEGORIZED values (6, 11, 3); // 
+insert into CATEGORIZED values (7, 0, 6); // 
+insert into CATEGORIZED values (8, 10, 5); // 
+insert into CATEGORIZED values (8, 12, 5); // 
+insert into CATEGORIZED values (9, 13, 2); // 
+insert into CATEGORIZED values (9, 15, 2); // 
+insert into CATEGORIZED values (10, 13, 5); // 
+insert into CATEGORIZED values (11, 16, 2); // 
+
 
 ------------------------------make F_DETAIL-------------------------------------------------
 
@@ -54,9 +105,13 @@ insert into F_DETAIL values(1, 5, '감사합니다', 'prose.jpg');
 insert into F_DETAIL values(2, 1, '기쁨의 순간', 'rgypsophila.jpg');
 insert into F_DETAIL values(3, 5, '자랑스럽다', 'pampas.jpg');
 insert into F_DETAIL values(4, 1, '축복합니다 축하합니다', 'poinsettia.jpg');
-
-
-
+insert into F_DETAIL values(5, 2, '반드시 오고야 말 행복', 'mery.jpg');
+insert into F_DETAIL values(6, 3, '슬픈 그대가 좋아요', 'long.jpg');
+insert into F_DETAIL values(7, 6, '희망', 'snow.jpg');
+insert into F_DETAIL values(8, 5, '근심을 풀어주세요', 'christ.jpg');
+insert into F_DETAIL values(9, 2, '당신의 시작을 응원해', 'freesia.jpg');
+insert into F_DETAIL values(10, 5, '새로운 시작', 'jak.jpg');
+insert into F_DETAIL values(11, 2, '돈을 불러주는 돈나무', 'money.jpg');
 
 
 
